a88b67e20f5b5ad1440b1bbe772b4cf8639b29d6
Mohamadi98 <mohamadinaena23@gmail.com>
2023-07-12 15:02:00 +0300

    adding server port

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from handlers.userHandler import userRouter
 from dotenv import load_dotenv
+import os
 from flask_cors import CORS
 
 load_dotenv()
+
+PORT = os.getenv('PORT')
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -13,3 +16,6 @@ app.register_blueprint(userRouter)
 @app.route('/')
 def index():
     return 'server running!'
+
+if __name__ == '__main__':
+    app.run(port=PORT)
