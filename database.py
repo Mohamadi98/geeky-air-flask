07265ad3615d6ed0bd7ae50ad9d3dfd36ae309bb
Mohamadi98 <mohamadinaena23@gmail.com>
2023-07-14 14:22:09 +0300

    restructuring the database

--- database.py
+++ database.py
@@ -22,7 +22,7 @@ def initialize():
     try:
         conn = psycopg2.connect(os.getenv('CONNECTION_STRING'))
         cur = conn.cursor()
-        cur.execute('CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, password TEXT UNIQUE NOT NULL, username TEXT NOT NULL, email TEXT UNIQUE NOT NULL, balance FLOAT NOT NULL DEFAULT 0, role TEXT DEFAULT %s);', ('user',))
+        cur.execute('CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, password TEXT NOT NULL, username TEXT NOT NULL, email TEXT UNIQUE NOT NULL, balance FLOAT NOT NULL DEFAULT 0, role TEXT DEFAULT %s);', ('user',))
         conn.commit()
         cur.execute('INSERT INTO users (username, email, password, balance, role) SELECT %s, %s, %s, %s, %s WHERE NOT EXISTS (SELECT 1 FROM users WHERE role = %s);', (os.getenv('ADMIN_USERNAME'), os.getenv('ADMIN_EMAIL'), adminPassHash(), 0.0, 'admin', 'admin'))
         conn.commit()
