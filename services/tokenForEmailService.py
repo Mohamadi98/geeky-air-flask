99a9081a65c4b81d927b21a41fdc9926e1d79f70
Mohamadi98 <mohamadinaena23@gmail.com>
2023-08-04 16:44:09 +0300

    fixes

--- services/tokenForEmailService.py
+++ services/tokenForEmailService.py
@@ -8,9 +8,10 @@ load_dotenv()
 def generate_token_for_email(email):
     # to set the expiry date of the token to 15 mins
     expiry = datetime.datetime.utcnow() + datetime.timedelta(minutes=15)
+    expiry_timestamp = int(expiry.timestamp())
     user = {
         'email': email,
-        'expiry': expiry
+        'expiry': expiry_timestamp
         }
     token = jwt.encode(user, os.getenv('SECRET_TOKEN'), algorithm='HS256')
 
